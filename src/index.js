fe784a5a5e0e85f721e99988c1a31796866e87fa
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-07-11 13:43:55 +0300

    clean up

--- src/index.js
+++ src/index.js
@@ -1,59 +1,30 @@
-function multiplyBigNumWithOneNum(a, b) {
-  const arr = a.split('').reverse();
-  let result = '';
-  let remainder = '0';
-  while (arr.length > 0) {
-    const num = arr.shift();
-    const intNum = parseInt(num, 10);
-    const intRemainder = parseInt(remainder, 10);
-    const res = (intNum * b) + intRemainder;
-    const resString = res.toString();
-    result = (resString.length === 2 && arr.length > 0 ? resString.substr(1) : resString) + result;
-    remainder = resString.length === 2 ? resString.substr(0, 1) : '0';
-  }
-  return result;
-}
-
-function multiplyBigNumbers(a, b) {
-  const arr = b.split('').reverse();
-  const resultArray = [];
-  arr.forEach((value, index) => {
-    const res = multiplyBigNumWithOneNum(a, value);
-    resultArray.push(`${res}${'0'.repeat(index)}`);
-  });
-  let result = '';
-  let remainder = '0';
-  const l = Math.max(...resultArray.map(val => val.length));
-  for (let i = 1; i <= l; i += 1) {
-    let sum = 0;
-    for (let g = 0; g < resultArray.length; g += 1) {
-      const num = resultArray[g];
-      if (i <= num.length) {
-        sum += parseInt(num.substr(-i, 1), 10);
-      }
-    }
-    sum += parseInt(remainder, 10);
-    let sumStr = sum.toString();
-    remainder = sumStr.length >= 2 ? sumStr.substr(0, sumStr.length - 1) : '0';
-    sumStr = sumStr.length >= 2 ? sumStr.substr(sumStr.length - 1) : sumStr;
-    result = sumStr + result;
-    sum = 0;
-  }
-  return (remainder + result).replace(/^0+/, '');
-}
-
 function multiply(a, b) {
-  const intA = parseInt(a, 10);
-  const intB = parseInt(b, 10);
-  let result = (intA * intB).toString();
-  if (result.indexOf('e+') > 0 || isNaN(parseInt(result, 10))) {
-    result = multiplyBigNumbers(a, b);
+  const aa = a.split('').reverse();
+  const bb = b.split('').reverse();
+
+  const stack = [];
+
+  for (let i = 0; i < aa.length; i += 1) {
+    for (let j = 0; j < bb.length; j += 1) {
+      const m = aa[i] * bb[j];
+      stack[i + j] = (stack[i + j]) ? stack[i + j] + m : m;
+    }
   }
-  return result;
+
+  for (let i = 0; i < stack.length; i += 1) {
+    const num = stack[i] % 10;
+    const move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1]) {
+      stack[i + 1] += move;
+    } else if (move !== 0) {
+      stack[i + 1] = move;
+    }
+  }
+
+
+  return stack.reverse().join('').replace(/^(0(?!$))+/, '');
 }
 
-export {
-  multiply,
-  multiplyBigNumbers,
-  multiplyBigNumWithOneNum,
-};
+export default multiply;
--- src/index.test.js
+++ src/index.test.js
@@ -1,51 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { expect } from 'chai';
-import { multiply, multiplyBigNumbers, multiplyBigNumWithOneNum } from './index';
-
-describe('multiplyBigNumbers', () => {
-  it('should return 30x69="2070"', () => {
-    expect(multiplyBigNumbers('30', '69')).to.be.equal('2070');
-  });
-  it('should return 1009x03="3027"', () => {
-    expect(multiplyBigNumbers('1009', '03')).to.be.equal('3027');
-  });
-  it('should return 98765x56894="5619135910"', () => {
-    expect(multiplyBigNumbers('98765', '56894')).to.be.equal('5619135910');
-  });
-});
-
-describe('multiplyBigNumWithOneNum', () => {
-  describe('Some simple multiplications', () => {
-    it('should return 2x3="6"', () => {
-      expect(multiplyBigNumWithOneNum('2', '3')).to.be.equal('6');
-    });
-    it('should return 99x9="891"', () => {
-      expect(multiplyBigNumWithOneNum('99', '9')).to.be.equal('891');
-    });
-    it('should return 999x9="8991"', () => {
-      expect(multiplyBigNumWithOneNum('999', '9')).to.be.equal('8991');
-    });
-    it('should return 1001x9="9009"', () => {
-      expect(multiplyBigNumWithOneNum('1001', '9')).to.be.equal('9009');
-    });
-    it('should return 1000x9="9000"', () => {
-      expect(multiplyBigNumWithOneNum('1000', '9')).to.be.equal('9000');
-    });
-  });
-  describe('Some big multiplications', () => {
-    it('should return 2830869077153280552556547081187254342445169156730"', () => {
-      expect(multiplyBigNumWithOneNum('2830869077153280552556547081187254342445169156730', '1')).to.be.equal('2830869077153280552556547081187254342445169156730');
-    });
-    const num = '58608473622772837728372827';
-    '7586374672263726736374'.split('').forEach((multiplier) => {
-      const expectedResult = parseInt(num, 10) * parseInt(multiplier, 10);
-      it(`should return ${expectedResult}`, () => {
-        const res = multiplyBigNumWithOneNum(num, multiplier);
-        expect(parseInt(res, 10)).to.be.equal(expectedResult);
-      });
-    });
-  });
-});
+import multiply from './index';
 
 describe('multiply', () => {
   describe('Some simple multiplications', () => {
